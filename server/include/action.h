b984171883fea36a4dbf40af2e6d79f1c848f735
Robineau Jules <jules.robineau@epitech.eu>
2019-06-17 15:13:27 +0200

    Insert all function

--- server/include/action.h
+++ server/include/action.h
@@ -19,29 +19,29 @@ typedef struct action {
 void forward(team_t *);
 void broadcast(team_t *);
 void right(team_t *);
+void incantation(team_t *);
+void left(team_t *);
+void look(team_t *);
+void inventory(team_t *);
+void my_fork(team_t *);
+void my_eject(team_t *);
+void take_object(team_t *);
+void set_object(team_t *);
 void connect_nbr(team_t *);
-// void left(team_t *);
-// void look(team_t *);
-// void inventory(team_t *);
-// void my_fork(team_t *);
-// void my_eject(team_t *);
-// void take_object(team_t *);
-// void set_object(team_t *);
-// void incantation(team_t *);
 
 static const action_t MY_ACTION[] = {
     {"Forward", 7, forward},
     {"Brodcast", 8, broadcast},
     {"Right", 5, right},
-    // {"Connect_nbr", 11, connect_nbr},
-    // {"Left", 4, left},
-    // {"Look", 4, look},
-    // {"Inventory", 9, inventory},
-    // {"Fork", 4, my_fork},
-    // {"Eject", 5, my_eject},
-    // {"Take object", 11, take_object},
-    // {"Set object", 10, set_object},
-    // {"Incantation", 11, incantation},
+    {"Incantation", 11, incantation},
+    {"Left", 4, left},
+    {"Look", 4, look},
+    {"Inventory", 9, inventory},
+    {"Fork", 4, my_fork},
+    {"Eject", 5, my_eject},
+    {"Take object", 11, take_object},
+    {"Set object", 10, set_object},
+    {"Connect_nbr", 11, connect_nbr},
 };
 
 #endif /* COMMAND_H_ */

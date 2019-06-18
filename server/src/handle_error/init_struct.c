d086e762361ac1dffae845502c5e0a7947097853
Robineau Jules <jules.robineau@epitech.eu>
2019-06-18 11:51:05 +0200

    HANDLE_CLT: Modification of files for norm and beginning of handle_clt
    Server must do:
    	- Create a queue of many client
    	- Know who is the client that is talking
    	- Handle of msg client to server
    	- handle og msg server to client

--- server/src/handle_error/init_struct.c
+++ server/src/handle_error/init_struct.c
@@ -15,7 +15,7 @@ void init_struct(info_game_t *info)
     info->width = -1;
     info->heigth = -1;
     info->nb_clt = -1;
-    info->freq = -1;
+    info->freq = 100;
     info->nb_team = 0;
     info->name_team = NULL;
 }
--- server/src/main.c
+++ server/src/main.c
@@ -12,7 +12,7 @@ int main(int ac, char **av, char **envp)
 {
     info_game_t info;
 
-	if (!envp[0] || ac < 13 || !handle_error(ac - 1, av + 1, &info))
-		return (print_usage(FAILURE));
-	return (server(&info));
+    if (!envp[0] || ac < 13 || !handle_error(ac - 1, av + 1, &info))
+        return (print_usage(FAILURE));
+    return (server(&info));
 }
